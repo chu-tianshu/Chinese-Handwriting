fd6118c90c1e02d466806e6cdf71c584b3b09d67
cts2003258 <chutianshu@tamu.edu>
2016-07-19 23:16:03 -0700

    Vision feedback

--- App2/VisionAssessor.cs
+++ App2/VisionAssessor.cs
@@ -7,7 +7,20 @@
             SketchPoint sampleCentroid = SketchFeatureExtraction.Centroid(sample);
             SketchPoint templateCentroid = SketchFeatureExtraction.Centroid(template);
 
-            double fc = Math.sqrt();
+            BoundingBox sampleBoundingBox = SketchFeatureExtraction.BoundingRectangle(sample);
+            BoundingBox templateBoundingBox = SketchFeatureExtraction.BoundingRectangle(template);
+
+            double sampleHeight = sampleBoundingBox.Height;
+            double templateHeight = templateBoundingBox.Height;
+
+            double sampleWidth = sampleBoundingBox.Width;
+            double templateWidth = templateBoundingBox.Width;
+
+            double fc = Math.Sqrt(Math.Pow(sampleCentroid.X - templateCentroid.X, 2) 
+                      + Math.Pow(sampleCentroid.Y - templateCentroid.Y, 2)) 
+                      / (0.5 * sampleWidth);
+            double fa = (sampleWidth * sampleHeight - templateWidth * templateHeight) / (templateWidth * templateHeight);
+            double fr = (sampleWidth / sampleHeight - templateWidth / templateHeight);
         }
 
         #region properties
